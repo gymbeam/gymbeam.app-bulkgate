55cfd7e4a3de39425ca65ec78c93d7e7a2d08524
aheldes <59527963+aheldes@users.noreply.github.com>
2022-08-03 09:39:15 +0200

    Initial load

--- src/component.py
+++ src/component.py
@@ -2,21 +2,46 @@
 Template Component main class.
 
 """
-import csv
+# import csv
+import sys
 import logging
-from datetime import datetime
+from dataclasses import dataclass
+import pandas as pd
+import requests
+# from datetime import datetime
 
 from keboola.component.base import ComponentBase
 from keboola.component.exceptions import UserException
 
 # configuration variables
-KEY_API_TOKEN = '#api_token'
-KEY_PRINT_HELLO = 'print_hello'
+KEY_CREDENTIALS = 'credentials'
+KEY_CREDENTIALS_APPLICATION_ID = '#application_id'
+KEY_CREDENTIALS_APPLICATION_TOKEN = '#application_token'
+
+KEY_SETTINGS = 'settings'
+KEY_SETTINGS_SENDER_ID = 'sender_id'
+KEY_SETTINGS_MESSAGE_TYPE = 'message_type'
+KEY_SETTINGS_VIBER = 'viber'
+KEY_SETTINGS_VIBER_SENDER = 'viber_sender'
 
 # list of mandatory parameters => if some is missing,
 # component will fail with readable message on initialization.
-REQUIRED_PARAMETERS = [KEY_PRINT_HELLO]
+REQUIRED_PARAMETERS = []
 REQUIRED_IMAGE_PARS = []
+
+
+@dataclass
+class Credentials:
+    application_id: str
+    application_token: str
+
+
+@dataclass
+class Settings:
+    sender_id: str
+    message_type: str
+    viber: bool
+    viber_sender: str
 
 
 class Component(ComponentBase):
@@ -38,39 +63,106 @@ class Component(ComponentBase):
         Main execution code
         """
 
-        # ####### EXAMPLE TO REMOVE
-        # check for missing configuration parameters
-        self.validate_configuration_parameters(REQUIRED_PARAMETERS)
-        self.validate_image_parameters(REQUIRED_IMAGE_PARS)
-        params = self.configuration.parameters
-        # Access parameters in data/config.json
-        if params.get(KEY_PRINT_HELLO):
-            logging.info("Hello World")
+        _credentials_object = self._parse_credentials_parameters()
+        _settings_object = self._parse_settings_parameters()
 
-        # get last state data/in/state.json from previous run
-        previous_state = self.get_state_file()
-        logging.info(previous_state.get('some_state_parameter'))
+        _url = self._get_url(_settings_object)
+        _body = self._get_body(_credentials_object, _settings_object)
 
-        # Create output table (Tabledefinition - just metadata)
-        table = self.create_out_table_definition('output.csv', incremental=True, primary_key=['timestamp'])
+        _data_in = self._parse_table()
 
-        # get file path of the table (data/out/tables/Features.csv)
-        out_table_path = table.full_path
-        logging.info(out_table_path)
+        self.send_messages(_url, _body, _data_in)
 
-        # DO whatever and save into out_table_path
-        with open(table.full_path, mode='wt', encoding='utf-8', newline='') as out_file:
-            writer = csv.DictWriter(out_file, fieldnames=['timestamp'])
-            writer.writeheader()
-            writer.writerow({"timestamp": datetime.now().isoformat()})
+    def _parse_credentials_parameters(self):
+        credentials = self.configuration.parameters.get(KEY_CREDENTIALS, {})
+        if credentials == {}:
+            logging.exception("Credentials configuration not specified.")
+            sys.exit(1)
+        try:
+            credentials_obj = Credentials(
+                credentials[KEY_CREDENTIALS_APPLICATION_ID],
+                credentials[KEY_CREDENTIALS_APPLICATION_TOKEN]
+                )
+        except KeyError as e:
+            logging.exception(f"Missing mandatory field {e} in credentials.")
+            sys.exit(1)
+        return credentials_obj
 
-        # Save table manifest (output.csv.manifest) from the tabledefinition
-        self.write_manifest(table)
+    def _parse_settings_parameters(self):
+        settings = self.configuration.parameters.get(KEY_SETTINGS, {})
+        if settings == {}:
+            logging.exception("Settings configuration not specified.")
+            sys.exit(1)
+        try:
+            settings_obj = Settings(
+                settings[KEY_SETTINGS_SENDER_ID],
+                settings[KEY_SETTINGS_MESSAGE_TYPE],
+                settings[KEY_SETTINGS_VIBER],
+                settings[KEY_SETTINGS_VIBER_SENDER]
+                )
+            if settings_obj.viber and settings_obj.viber_sender == '':
+                logging.exception(
+                    f"Missing mandatory field {KEY_SETTINGS_VIBER_SENDER} in settings. "
+                    f"Please specify {KEY_SETTINGS_VIBER_SENDER} for Viber."
+                    )
+                sys.exit(1)
+        except KeyError as e:
+            logging.exception(f"Missing mandatory field {e} in settings.")
+            sys.exit(1)
+        return settings_obj
 
-        # Write new state - will be available next run
-        self.write_state_file({"some_state_parameter": "value"})
+    def _get_url(self, settings):
+        if settings.message_type == 'Promotional':
+            return "https://portal.bulkgate.com/api/2.0/advanced/promotional"
+        return "https://portal.bulkgate.com/api/2.0/advanced/transactional"
 
-        # ####### EXAMPLE TO REMOVE END
+    def _get_body(self, credentials, settings):
+        channels = {}
+        if settings.viber:
+            channels['viber'] = {"sender": settings.viber_sender}
+        channels['sms'] = {"sender_id": "gProfile", "sender_id_value": settings.sender_id}
+        return {"application_id": credentials.application_id,
+                "application_token": credentials.application_token,
+                "number": [],
+                "channel": channels
+                }
+
+    def _parse_table(self):
+        in_tables = self.configuration.tables_input_mapping
+
+        if len(in_tables) == 0:
+            logging.exception('There is no table specified on the input mapping! You must provide one input table!')
+            exit(1)
+        elif len(in_tables) > 1:
+            logging.exception(
+                'There is more than one table specified on the input mapping! You must provide one input table!')
+            exit(1)
+
+        table = in_tables[0]
+
+        logging.info(f'Processing input table: {table["destination"]}')
+        return pd.read_csv(f'{self.tables_in_path}/{table["destination"]}', dtype=str)
+
+    def send_messages(self, url, body, data):
+        """
+        Send messages to the specified recipients
+        """
+        timestamps = data.timestamp.unique()
+
+        for time in timestamps:
+            number = []
+            for message in data.loc[data.timestamp == time].to_dict('records'):
+                number.append(
+                    {"number": message['number'], "text": message['text']}
+                )
+            body['number'] = number
+
+            if time != "0":
+                body['schedule'] = time
+
+            response = requests.post(url, json=body)
+            stats = response.json()['data']['total']['status']
+            logging.info(stats)
 
 
 """
