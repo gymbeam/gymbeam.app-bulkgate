c6025063f85742e75626f8df239b7ffcb5bc990b
Adam Bako <bakoad@gmail.com>
2023-03-29 14:11:13 +0200

    New functionality
    * swapped send to viber with a specific channel select
    * added option to add custom buttons to viber messages
    * fixed parsing of messages when an invalid number is provided
    * added duplicate check option
    * added unicode sms option

--- component_config/configuration_description.md
+++ component_config/configuration_description.md
@@ -1,11 +1,13 @@
 ## Input
 
-Input should be a table with 3 columns:
+Input should be a table with 5 columns:
 
  - `number` - phone number to which message will be send - must include country code (ex: 420123123123)
  - `text` - text which will be send
  - `timestamp` - `0` for message to be sent right away or [unix timestamp](https://en.wikipedia.org/wiki/Unix_time)/[ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) for message to be sent at a specific time
-
+ - `button_caption` - Caption for the [Button object](https://help.bulkgate.com/docs/en/http-advanced-promotional-v2.html#button-object-parameters-table) to be sent with a viber message. Can be null for sms. button_url must also be provided
+ - `button_url` - URL for the [Button object](https://help.bulkgate.com/docs/en/http-advanced-promotional-v2.html#button-object-parameters-table) to be sent with a viber message. Can be null for sms. button_caption must also be provided
+ 
 ## Credentials
 
 Credentials used for authentification:
@@ -21,5 +23,7 @@ Additional settings for message sending:
 
  - `Message type` - can be either `Transactional` or `Promotional` depending on the purpose of the message. Be aware that as per [documentation](https://help.bulkgate.com/docs/en/difference-promotional-transactional-sms.html#transactional-sms) **it is *strictly prohibited* to exploit transactional SMS for promotional/marketing uses. It must be used for notification purposes only - as an SMS notification.** 
  - `Sender ID` - [sender ID profile](https://help.bulkgate.com/docs/en/sender-id-profile.html)
- - `Send to Viber?` - if `true` message will be send to `viber` primarly and fallback to `SMS` if the provided phone number does not have `viber` account. If `false` message will be send only via `SMS`
- - `Viber sender` - sender name which will be shown in `viber` as the sender of a message (this field is mandatory if `Send to Viber?` is `true`
+ - `Sending Option?` - Determines which channels will be used for sending the message: "viber" for viber only, "sms" for sms only, "viber_sms" for viber and sms as backup
+ - `Viber sender` - Sender name which will be shown in `viber` as the sender of a message (this field is mandatory if `send_options` is `viber_sms` or `viber`
+ - `SMS Unicode` - If set to true, messages via sms will be sent in unicode (characters with diacritics will be sent properly) .
+ - `Duplicates Check` - Select 'same_text' to prevent sending duplicate messages to the same phone number. Disable the possibility to send a message with either the same or different text to the same number with 'same_number'. If 'null' no duplicates will be removed.
--- src/component.py
+++ src/component.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import logging
 import pandas as pd
 import requests
-import sys
+import copy
 
 from keboola.component.base import ComponentBase
 from keboola.component.exceptions import UserException
@@ -22,12 +22,13 @@ KEY_CREDENTIALS_APPLICATION_TOKEN = '#application_token'
 KEY_SETTINGS = 'settings'
 KEY_SETTINGS_SENDER_ID = 'sender_id'
 KEY_SETTINGS_MESSAGE_TYPE = 'message_type'
-KEY_SETTINGS_VIBER = 'viber'
+KEY_SETTINGS_UNICODE = "sms_unicode"
+KEY_SETTINGS_DUPLICATES_CHECK = "duplicates_check"
+KEY_SETTINGS_CHANNEL = 'send_options'
 KEY_SETTINGS_VIBER_SENDER = 'viber_sender'
 
 # set urls
 PROMOTIONAL_URL = "https://portal.bulkgate.com/api/2.0/advanced/promotional"
-TRANSACTIONAL_URL = "https://portal.bulkgate.com/api/2.0/advanced/transactional"
 
 
 @dataclass
@@ -46,8 +47,10 @@ class Settings:
     """
     sender_id: str
     message_type: str
-    viber: bool
+    send_options: str
     viber_sender: str
+    unicode: bool
+    duplicates_check: str
 
 
 class Component(ComponentBase):
@@ -77,13 +80,15 @@ class Component(ComponentBase):
         _data_in = self._parse_table()
 
         # Get url and body
-        _url = self._get_url(_settings_object)
+        _url = self._get_url()
         _body = self._get_body(_credentials_object, _settings_object)
+
+        self._create_tables_definitions()
 
         # Send messages
         self._send_messages(_url, _body, _data_in)
 
-        # Close files and manifes data
+        # Close files and manifest data
         self._close_and_manifest_files()
 
     def _parse_credentials_parameters(self):
@@ -94,18 +99,16 @@ class Component(ComponentBase):
         # Get credentials
         credentials = self.configuration.parameters.get(KEY_CREDENTIALS, {})
         if credentials == {}:
-            logging.exception("Credentials configuration not specified.")
-            sys.exit(1)
+            raise UserException("Credentials configuration not specified.")
 
         try:
             # Set credentials object
             credentials_obj = Credentials(
                 credentials[KEY_CREDENTIALS_APPLICATION_ID],
                 credentials[KEY_CREDENTIALS_APPLICATION_TOKEN]
-                )
+            )
         except KeyError as e:
-            logging.exception(f"Missing mandatory field {e} in credentials.")
-            sys.exit(1)
+            raise UserException(f"Missing mandatory field {e} in credentials.") from e
 
         return credentials_obj
 
@@ -117,29 +120,31 @@ class Component(ComponentBase):
         # Get settings
         settings = self.configuration.parameters.get(KEY_SETTINGS, {})
         if settings == {}:
-            logging.exception("Settings configuration not specified.")
-            sys.exit(1)
+            raise UserException("Settings configuration not specified.")
 
         try:
             # Set settings object
             settings_obj = Settings(
                 settings[KEY_SETTINGS_SENDER_ID],
                 settings[KEY_SETTINGS_MESSAGE_TYPE],
-                settings[KEY_SETTINGS_VIBER],
-                settings[KEY_SETTINGS_VIBER_SENDER]
-                )
+                settings.get(KEY_SETTINGS_CHANNEL, "viber"),
+                settings[KEY_SETTINGS_VIBER_SENDER],
+                settings.get(KEY_SETTINGS_UNICODE, False),
+                settings.get(KEY_SETTINGS_DUPLICATES_CHECK)
+            )
 
             # Validate settings
-            if settings_obj.viber and settings_obj.viber_sender == '':
-                logging.exception(
+            if settings_obj.send_options in ["viber", "viber_sms"] and settings_obj.viber_sender == '':
+                raise UserException(
                     f"Missing mandatory field {KEY_SETTINGS_VIBER_SENDER} in settings. "
                     f"Please specify {KEY_SETTINGS_VIBER_SENDER} for Viber."
-                    )
-                sys.exit(1)
+                )
+            if settings_obj.duplicates_check and settings_obj.duplicates_check not in ["same_text", "same_number",
+                                                                                       "null"]:
+                raise UserException("Value of Duplicates Check must be one of : [\"same_text\", \"same_number\"]")
 
         except KeyError as e:
-            logging.exception(f"Missing mandatory field {e} in settings.")
-            sys.exit(1)
+            raise UserException(f"Missing mandatory field {e} in settings.") from e
 
         return settings_obj
 
@@ -149,90 +154,136 @@ class Component(ComponentBase):
         """
 
         # Get tables configuration
-        in_tables = self.configuration.tables_input_mapping
+        in_tables = self.get_input_tables_definitions()
 
         if len(in_tables) == 0:
-            logging.exception('There is no table specified on the input mapping! You must provide one input table!')
-            sys.exit(1)
+            raise UserException('There is no table specified on the input mapping! You must provide one input table!')
         elif len(in_tables) > 1:
-            logging.exception(
+            raise UserException(
                 'There is more than one table specified on the input mapping! You must provide one input table!')
-            sys.exit(1)
 
         # Get table
         table = in_tables[0]
 
         # Get table data
-        logging.info(f'Processing input table: {table["destination"]}')
-        df = pd.read_csv(f'{self.tables_in_path}/{table["destination"]}', dtype=str)
+        logging.info(f'Processing input table: {table.name}')
+        df = pd.read_csv(f'{table.full_path}', dtype=str)
 
         # Return error if there is no data
         if df.empty:
-            logging.exception(f'Input table {table["destination"]} is empty!')
-            exit(1)
+            raise UserException(f'Input table {table.name} is empty!')
 
         return df
 
-    def _get_url(self, settings: Settings) -> str:
+    @staticmethod
+    def _get_url() -> str:
         """
         Returns the url for the request.
         """
-        if settings.message_type == 'Promotional':
-            return PROMOTIONAL_URL
-        return TRANSACTIONAL_URL
+        return PROMOTIONAL_URL
 
-    def _get_body(self, credentials: Credentials, settings: Settings) -> dict:
+    @staticmethod
+    def _get_body(credentials: Credentials, settings: Settings) -> dict:
         """
         Returns the body for the request.
         """
 
         # Set channels
         channels = {}
-        if settings.viber:
+        if settings.send_options in ["viber", "viber_sms"]:
             channels['viber'] = {"sender": settings.viber_sender}
 
-        channels['sms'] = {
-            "sender_id": "gProfile",
-            "sender_id_value": str(settings.sender_id)
+        if settings.send_options in ["sms", "viber_sms"]:
+            channels['sms'] = {
+                "sender_id": "gProfile",
+                "sender_id_value": str(settings.sender_id),
+                "unicode": settings.unicode
             }
 
-        return {"application_id": credentials.application_id,
+        body = {"application_id": credentials.application_id,
                 "application_token": credentials.application_token,
                 "number": [],
-                "channel": channels
-                }
+                "channel": channels}
+
+        if settings.duplicates_check:
+            body["duplicates_check"] = settings.duplicates_check
+
+        return body
+
+    @staticmethod
+    def create_groups_of_dataframes_that_share_the_same_column_values(data: pd.DataFrame, columns: list) -> list[
+        pd.DataFrame]:
+        return [sub_df for group, sub_df in data.groupby(columns, dropna=False)]
+
+    @staticmethod
+    def get_texts_from_dataframe(df: pd.DataFrame) -> list[dict]:
+        return [{"number": row.number, "text": row.text} for index, row in df.iterrows()]
+
+    @staticmethod
+    def _get_non_changing_param_from_dataframe(df: pd.DataFrame, param_name: str):
+        """
+        This method gets a specific column from a dataframe, where the column value should be the same for each row.
+        If a specific row contains a different value for the column a UserException is raised
+        """
+        param = None
+        for index, row in df.iterrows():
+            if param and param != row[param_name]:
+                raise UserException(f"Grouping of {param_name} have failed, it is not possible to send a single"
+                                    f" request with multiple texts that contain different {param_name}s")
+            param = row[param_name]
+        return param
 
     def _send_messages(self, url: str, body: dict, data: pd.DataFrame) -> None:
         """
         Send messages to the specified recipients
         """
-        # Get unique timestamps
-        timestamps = data.timestamp.unique()
-        runs = 0
 
-        for time in timestamps:
-            # Get the recipients and messages for the current timestamp
-            number = []
-            for message in data.loc[data.timestamp == time].to_dict('records'):
-                number.append({"number": message['number'], "text": message['text']})
-            body['number'] = number
+        if 'button_caption' not in data.columns:
+            if "viber" in body["channel"]:
+                logging.warning("button_caption column is missing in data, not adding buttons to viber messages")
+            data['button_caption'] = None
+
+        if 'button_url' not in data.columns:
+            if "viber" in body["channel"]:
+                logging.warning("button_url column is missing in data, not adding buttons to viber messages")
+            data['button_url'] = None
+
+        # Create button_object column
+        data['button_object'] = data.apply(lambda x: {'caption': x['button_caption'], 'url': x['button_url']}, axis=1)
+
+        # Create add_button column that is a boolean indicator if a button has valid params
+        data['add_button'] = data['button_caption'].notnull() & data['button_url'].notnull()
+
+        # Each request has to have the same timestamp/schedule as well as the same button, as the button is defined in
+        # the channel. Each number+text that has the same schedule and same button can be sent in a single request
+        # with the number and text in the "number" parameter of the body. The below code is used to create a list of
+        # dataframes that share the same button and schedule.
+        send_groups = self.create_groups_of_dataframes_that_share_the_same_column_values(data, ['button_url',
+                                                                                                'button_caption',
+                                                                                                "timestamp"])
+
+        for group in send_groups:
+            final_body = copy.deepcopy(body)
+
+            final_body['number'] = self.get_texts_from_dataframe(group)
+            timestamp = self._get_non_changing_param_from_dataframe(group, "timestamp")
+
+            button = self._get_non_changing_param_from_dataframe(group, "button_object")
+            add_button = self._get_non_changing_param_from_dataframe(group, "add_button")
+
+            if "viber" in final_body["channel"] and add_button:
+                final_body["channel"]['viber']["button"] = button
 
             # Set schedule if present
-            if time != "0":
-                body['schedule'] = time
+            if timestamp != "0":
+                final_body['schedule'] = timestamp
 
-            # Send the request
-            response = requests.post(url, json=body)
+            response = requests.post(url, json=final_body)
 
             # Return error if the request fails
             if response.status_code != 200:
-                logging.error(f'There was an error calling Bulkgate API due to {response.reason}')
-                sys.exit(1)
+                raise UserException(f'There was an error calling Bulkgate API due to {response.reason}')
 
-            # Save response
-            if runs == 0:
-                runs += 1
-                self._create_tables_definitions()
             self._save_response(response.json())
 
     def _create_tables_definitions(self):
@@ -246,13 +297,15 @@ class Component(ComponentBase):
             'messages.csv', incremental=True, primary_key=['message_id'])
         self._messages_parts_table = self.create_out_table_definition(
             'messages_parts.csv', incremental=True, primary_key=['part_id'])
+        self._invalid_number_errors_table = self.create_out_table_definition(
+            'invalid_number_errors.csv', incremental=True, primary_key=['number', 'timestamp'])
 
         # Open stats file, set headers, writer and write headers
         self._stats_file = open(self._stats_table.full_path, 'wt', encoding='UTF-8', newline='')
         stats_fields = [
-                'timestamp', 'sent', 'accepted', 'scheduled',
-                'error', 'blacklisted', 'invalid_number', 'invalid_sender'
-                ]
+            'timestamp', 'sent', 'accepted', 'scheduled',
+            'error', 'blacklisted', 'invalid_number', 'invalid_sender'
+        ]
         self._stats_writer = csv.DictWriter(self._stats_file, fieldnames=stats_fields)
         self._stats_writer.writeheader()
 
@@ -267,6 +320,13 @@ class Component(ComponentBase):
         messages_parts_fields = ['part_id', 'message_id']
         self._messages_parts_writer = csv.DictWriter(self._messages_parts_file, fieldnames=messages_parts_fields)
         self._messages_parts_writer.writeheader()
+
+        self._invalid_number_errors_file = open(self._invalid_number_errors_table.full_path, 'wt', encoding='UTF-8',
+                                                newline='')
+        _invalid_number_errors_fields = ["number", "status", "code", "error", "detail", "channel", "timestamp"]
+        self._invalid_number_errors_writer = csv.DictWriter(self._invalid_number_errors_file,
+                                                            fieldnames=_invalid_number_errors_fields)
+        self._invalid_number_errors_writer.writeheader()
 
     def _save_response(self, response: dict) -> None:
         """
@@ -284,17 +344,19 @@ class Component(ComponentBase):
         # Parse messages data and load
         for message in response['data']['response']:
             message['timestamp'] = current_time
+            if message.get("status") == "invalid_number":
+                self._invalid_number_errors_writer.writerow(message)
+            else:
+                # Load messages parts data
+                for part in message['part_id']:
+                    self._messages_parts_writer.writerow({
+                        'part_id': part,
+                        'message_id': message['message_id']
+                    })
 
-            # Load messages parts data
-            for part in message['part_id']:
-                self._messages_parts_writer.writerow({
-                    'part_id': part,
-                    'message_id': message['message_id']
-                })
-
-            # Load messages data
-            message.pop('part_id', None)
-            self._messages_writer.writerow(message)
+                # Load messages data
+                message.pop('part_id', None)
+                self._messages_writer.writerow(message)
 
     def _close_and_manifest_files(self):
         """
@@ -304,9 +366,11 @@ class Component(ComponentBase):
         self._stats_file.close()
         self._messages_file.close()
         self._messages_parts_file.close()
+        self._invalid_number_errors_file.close()
 
         # Manifest files
-        self.write_manifests([self._stats_table, self._messages_table, self._messages_parts_table])
+        self.write_manifests(
+            [self._stats_table, self._messages_table, self._messages_parts_table, self._invalid_number_errors_table])
 
 
 """
